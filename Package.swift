d52ce8a4524a6b233a4863653546532ed0886043
Paul Nicholson <paulnicholson@users.noreply.github.com>
2026-04-28 11:29:48 -0400

    Change dependency versioning for crowdconnected-sdk
    Pin to exact version 3.0.2 which resolves a conflict with SQLCipher

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/crowdconnected/crowdconnected-sdk-swift-spm", exact: "3.0.1"),
+        .package(url: "https://github.com/crowdconnected/crowdconnected-sdk-swift-spm", from: "3.0.2"),
         .package(url: "https://github.com/expofp/expofp-sdk-ios", from: "5.6.0"),
     ],
     targets: [
